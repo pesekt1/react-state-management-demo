02ed1e7a1627cb5a3bd68388b7f43b01f8694237
Tomas Pesek <pesekt@gmail.com>
2024-04-22 09:15:39 +0200

    created counter reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
-import './App.css';
+import "./App.css";
+import Counter from "./components/Counter";
 
 function App() {
-  return <h1>React Starter Project</h1>;
+  return (
+    <>
+      <h1>React State Management Demo</h1>
+      <Counter />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/reducers/counterReducer.ts
@@ -0,0 +1,16 @@
+interface Action {
+  type: "INCREMENT" | "RESET";
+}
+
+const counterReducer = (state: number, action: Action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "RESET":
+      return 0;
+    default:
+      return state;
+  }
+};
+
+export default counterReducer;
